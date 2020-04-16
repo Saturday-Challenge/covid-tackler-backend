fe99ad91e4a0bfc3d1a0f8749a4db07b2b948bec
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2020-04-16 23:46:40 +0530

    Update usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -4,7 +4,8 @@ var schema = mongoose.Schema;
 var userschema = new schema({
     name : {type:String , unique:true, required:true},
     password : {type:String ,required:true},
-    email : {type:String , unique:true, required :true}
+    email : {type:String , unique:true, required :true},
+    usertype :{type :String , required:true}
 })
 
 module.exports = mongoose.model('users',userschema)

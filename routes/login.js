3b9d1e21bade349769aace3de0ae3153a92973b5
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2020-04-16 14:48:46 +0530

    Create login.js

---
+++ routes/login.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var bodyparser = require('body-parser')
+
+var users = require('../models/usermodel.js')
+var router = express.Router()
+
+router.use(express.static(__dirname))
+router.use(bodyparser.urlencoded({extended:true}))
+router.use(bodyparser.json())
+
+
+function user_login(req,res)
+{
+    var name = req.body.name
+    var password = req.body.password
+    // console.log(name,password)
+
+    users.findOne({$and : [{password : password} , {name : name}]} , {name:0,email:0,__v:0})
+        .then(result=>
+            {
+                if(!result)
+                    res.sendStatus(404)
+                else
+                    res.send("Welcome "+name)
+                    return __id
+            })
+        .catch(err=>
+            {
+                console.log(err)
+            })
+}
+
+
+router.get('/',(req,res)=>
+{
+    res.sendfile('login.html')
+});
+
+router.post('/',user_login);
+
+module.exports = {LoginRoute : router}
